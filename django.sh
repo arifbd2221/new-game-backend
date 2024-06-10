a6f032a6d26394a74e8b3ef10148b94f1bd6b11f
arif <mohidul.haque@goama.com>
2024-06-10 16:41:25 +0600

    Added gunicorn

--- django.sh
+++ django.sh
@@ -4,4 +4,4 @@ python manage.py migrate
 echo "=================================="
 
 echo "Start server"
-python manage.py runserver 0.0.0.0:8000
+gunicorn backend.wsgi:application --bind 0.0.0.0:8000
